10123ead4f9bbae02b6c289c83cbd0c125258c73
Julien Malcouronne <malcouronnejulien@gmail.com>
2021-12-29 10:27:30 +0100

    breedlist done

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import useBreedList from "./useBreedList";
 import Pet from "./Pet";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
@@ -8,7 +9,7 @@ const SearchParams = () => {
   const [animal, updateAnimal] = useState("");
   const [breed, updateBreed] = useState("");
   const [pets, setPets] = useState([]);
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
@@ -26,7 +27,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+      onSubmit= {(e) => {
+        e.preventDefault();
+        requestPets();
+      }}
+      >
         <label htmlFor="location">
           Location
           <input
