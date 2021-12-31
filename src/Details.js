8260548f426bd583d58bfd7d890761a7dd2238f2
Julien Malcouronne <malcouronnejulien@gmail.com>
2021-12-31 09:11:53 +0100

    view done for each animals

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,46 @@
-const Details = () => {
-  return <h2>yo</h2>;
+import { Component } from 'react';
+import { withRouter } from 'react-router-dom'
+
+class Details extends Component {
+  constructor() {
+    super();
+    this.state = { loading: true};
+  }
+  async componentDidMount() {
+    const res = await fetch(
+      //match params permit to get the id of the animal
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    )
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+      {
+      loading: false,
+      // name: json.pets[0].name for each thing we need or
+      // breed: json.pets[0].breed, for each item
+      },
+      json.pets[0]
+      )
+    );
+  }
+
+  render() {
+    console.log(this.state);
+    const { animal, breed, city, state, description, name } = this.state;
+    return(
+      <div className='details'>
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${city}, ${state}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+
+      </div>
+
+    )
+  }
 }
 
-export default Details;
+
+export default withRouter(Details);
