65b55c828b636b21d40323ab18124afb639b9f6f
Julien Malcouronne <malcouronnejulien@gmail.com>
2021-12-31 08:35:07 +0100

    React Router done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import ReactDOM from "react-dom";
-// import { render } from "react-dom";
+import Details from "./Details"
 import SearchParams from "./SearchParams";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { StrictMode } from "react";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>yo</h2>;
+}
+
+export default Details;
