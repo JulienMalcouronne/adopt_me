16989f7789dae043edca71b5c9b29c3503c85054
Julien Malcouronne <malcouronnejulien@gmail.com>
2021-12-31 13:12:15 +0100

    details page protected from errors

--- src/Details.js
+++ src/Details.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import Carousel from "./Carousel";
+import ErrorBoundary from './ErrorBoundary';
 
 class Details extends Component {
   state = { loading: true };
@@ -49,5 +50,11 @@ class Details extends Component {
   }
 }
 
-
-export default withRouter(Details);
+const DetailsWithRouter = withRouter(Details);
+export default function DetailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  )
+};
---
+++ src/ErrorBoundary.js
@@ -0,0 +1,29 @@
+import { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class ErrorBoundary extends Component {
+  state = { hasError: false };
+  static getDerivedStateFromError(e) {
+    return { hasError: true}
+  }
+  componentDidCatch(error, info) {
+    // this work with Sentry, Azure Monitor, New Relic, TrackJS
+    console.error("ErrorBoundary caught an error", error, info);
+  }
+  render() {
+    if (this.state.hasError) {
+      return(
+        <h2>
+          this listing has an error. <Link to="/">Click here</Link> to go to the home page.
+        </h2>
+      )
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
+
+/* <ErrorBoundary>
+  <h1>Hi there</h1>
+</ErrorBoundary> */
