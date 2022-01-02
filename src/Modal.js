f819f0597535383ecd1878e22b8172cef4c0173b
Julien Malcouronne <malcouronnejulien@gmail.com>
2022-01-02 11:03:28 +0100

    done with modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,13 +1,12 @@
-import { useEffect, useRef } from "react";
-import { createPortal } from 'react-dom'
+import React, { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
 
-const modalRoot = document.getElementById('modal');
-// const container = document.createElement('div');
+const modalRoot = document.getElementById("modal");
 
 const Modal = ({ children }) => {
   const elRef = useRef(null);
   if (!elRef.current) {
-    elRef.current = document.createElement('div');
+    elRef.current = document.createElement("div");
   }
 
   useEffect(() => {
@@ -16,6 +15,6 @@ const Modal = ({ children }) => {
   }, []);
 
   return createPortal(<div>{children}</div>, elRef.current);
-}
+};
 
 export default Modal;
